035778abe5dcd2fbd152fe620240d052f2f40cb5
mistsy <michal.steff@gmail.com>
2025-09-04 16:37:01 +0200

    Slider

--- scripts.js
+++ scripts.js
@@ -1,11 +1,10 @@
 'use strict';
-
 const mobileMenuToggler = document.querySelector('#menu-toggle');
 const mobileMenuWrapper = document.querySelector('#nav_wrapper');
 const navigationElements = mobileMenuWrapper.querySelectorAll('a');
 
 const mobileMenuHandler = (toggler) => {
-  toggler.addEventListener('click', function () {
+	toggler.addEventListener('click', function () {
 		if (mobileMenuToggler.classList.contains('active')) {
 			mobileMenuToggler.classList.remove('active');
 			mobileMenuWrapper.classList.remove('active');
@@ -16,7 +15,19 @@ const mobileMenuHandler = (toggler) => {
 	});
 };
 
-mobileMenuToggler.addEventListener('click', mobileMenuHandler(mobileMenuToggler));
-navigationElements.forEach((element) => element.addEventListener('click', mobileMenuHandler(element)));
+mobileMenuToggler.addEventListener(
+	'click',
+	mobileMenuHandler(mobileMenuToggler),
+);
+navigationElements.forEach((element) =>
+	element.addEventListener('click', mobileMenuHandler(element)),
+);
 
+const glideOptions = {
+	type: 'carousel',
+	perView: 1,
+	autoplay: 4000
+};
 
+new Glide('.glide1', glideOptions).mount();
+new Glide('.glide2', glideOptions).mount();
